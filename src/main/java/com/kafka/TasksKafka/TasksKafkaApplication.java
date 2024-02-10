a87d2f057aad1898f0c1ca01b6a578f47cdafe1d
robertperquim <perquimrobert@gmail.com>
2024-02-10 05:32:36 -0300

    Feat. Create Project

---
+++ src/main/java/com/kafka/TasksKafka/TasksKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.TasksKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksKafkaApplication.class, args);
+	}
+
+}
