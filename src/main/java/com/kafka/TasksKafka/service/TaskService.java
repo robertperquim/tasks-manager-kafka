a7269cf2f3862ecf382b796458c5757370ae5ef0
robertperquim <perquimrobert@gmail.com>
2024-02-16 03:05:07 -0300

    feat/Adicionando logger na service

--- src/main/java/com/kafka/TasksKafka/service/TaskService.java
+++ src/main/java/com/kafka/TasksKafka/service/TaskService.java
@@ -3,7 +3,10 @@ import com.kafka.TasksKafka.model.Task;
 import com.kafka.TasksKafka.model.recordclasses.TaskDetalingData;
 import com.kafka.TasksKafka.model.recordclasses.TaskRegisterData;
 import com.kafka.TasksKafka.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskService.class);
 
     @Autowired
     private TaskRepository taskRepository;
@@ -32,6 +36,7 @@ public class TaskService {
     public Mono<TaskDetalingData> createTask(TaskRegisterData data) {
         Task newTask = new Task(data);
         return Mono.fromCallable(() -> taskRepository.save(newTask))
+                .doOnNext(it -> LOGGER.info("Saved task with title {}", it.getTitle()))
                 .map(task -> new TaskDetalingData(newTask));
     }
 
