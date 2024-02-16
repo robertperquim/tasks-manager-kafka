eb0d89b4b6127e84975d4c683d4b837ba8111a70
robertperquim <perquimrobert@gmail.com>
2024-02-16 05:49:11 -0300

    feat/ adicionando construtor para novo modo de paginacao

--- src/main/java/com/kafka/TasksKafka/model/recordclasses/TaskDetalingData.java
+++ src/main/java/com/kafka/TasksKafka/model/recordclasses/TaskDetalingData.java
@@ -9,4 +9,10 @@ public record TaskDetalingData(String id, String title, String description, int 
         this(task.getId(), task.getTitle(), task.getDescription(), task.getPriority());
     }
 
+
+    public static Task toTask(String id, String title, String description, int priority) {
+        return new Task(id, title, description, priority );
+    }
+
+
 }
